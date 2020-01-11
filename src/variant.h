83951f84026a56f10a1c0037c98f482590aaaee3
Håkan Sidenvall <ekcoh.git@gmail.com>
2020-01-11 01:12:04 +0100

    Fixed formatting

--- src/variant.h
+++ src/variant.h
@@ -18,43 +18,43 @@ namespace gb2gc
 
    inline constexpr nonstd::monostate null_value() { return {}; }
 
-	using variant = nonstd::variant<
+   using variant = nonstd::variant<
       null_type,           // 0
-		short,               // 1
-		unsigned short,      // 2
-		int,                 // 3
-		unsigned int,        // 4
-		long,                // 5
-		unsigned long,       // 6
-		long long,           // 7
-		unsigned long long,  // 8
-		float,               // 9
-		double,              // 10
-		long double,         // 11
-		std::string>;        // 12
+      short,               // 1
+      unsigned short,      // 2
+      int,                 // 3
+      unsigned int,        // 4
+      long,                // 5
+      unsigned long,       // 6
+      long long,           // 7
+      unsigned long long,  // 8
+      float,               // 9
+      double,              // 10
+      long double,         // 11
+      std::string>;        // 12
 
-	inline std::ostream& operator<<(std::ostream & os, gb2gc::variant const & v)
-	{
-		switch (v.index())
-		{
-		case 0:  os << "'null'"; break;
-		case 1:  os << std::to_string(nonstd::get<1>(v)); break;
-		case 2:  os << std::to_string(nonstd::get<2>(v)); break;
-		case 3:  os << std::to_string(nonstd::get<3>(v)); break;
-		case 4:  os << std::to_string(nonstd::get<4>(v)); break;
-		case 5:  os << std::to_string(nonstd::get<5>(v)); break;
-		case 6:  os << std::to_string(nonstd::get<6>(v)); break;
-		case 7:  os << std::to_string(nonstd::get<7>(v)); break;
-		case 8:  os << std::to_string(nonstd::get<8>(v)); break;
-		case 9:  os << std::to_string(nonstd::get<9>(v)); break;
-		case 10: os << std::to_string(nonstd::get<10>(v)); break;
-		case 11: os << std::to_string(nonstd::get<11>(v)); break;
-		case 12: os << '\'' << nonstd::get<12>(v) << '\''; break;
-		default: 
+   inline std::ostream& operator<<(std::ostream & os, gb2gc::variant const & v)
+   {
+      switch (v.index())
+      {
+      case 0:  os << "'null'"; break;
+      case 1:  os << std::to_string(nonstd::get<1>(v)); break;
+      case 2:  os << std::to_string(nonstd::get<2>(v)); break;
+      case 3:  os << std::to_string(nonstd::get<3>(v)); break;
+      case 4:  os << std::to_string(nonstd::get<4>(v)); break;
+      case 5:  os << std::to_string(nonstd::get<5>(v)); break;
+      case 6:  os << std::to_string(nonstd::get<6>(v)); break;
+      case 7:  os << std::to_string(nonstd::get<7>(v)); break;
+      case 8:  os << std::to_string(nonstd::get<8>(v)); break;
+      case 9:  os << std::to_string(nonstd::get<9>(v)); break;
+      case 10: os << std::to_string(nonstd::get<10>(v)); break;
+      case 11: os << std::to_string(nonstd::get<11>(v)); break;
+      case 12: os << '\'' << nonstd::get<12>(v) << '\''; break;
+      default:
          throw std::out_of_range("variant index out of range");
-		}
-		return os;
-	}
+      }
+      return os;
+   }
 
    inline std::string to_string(const gb2gc::variant& value)
    {
